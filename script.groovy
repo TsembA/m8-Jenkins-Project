4792d92dfa50ce1b737e907f8151dbb9dceab4b7
Tsemb <Tsembenhoi@gmail.com>
2025-02-26 21:04:40 +0200

    Add docker file and update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,17 +1,3 @@
-def buildJar() {
-    echo 'building the application...'
-    sh 'mvn package'
-}
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t dancedevops/my-app:jma-1.2.3 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push dancedevops/my-app:jma-1.2.3'
-    }
-}
-
 def deployApp() {
     echo 'deploying the application...'
 }
